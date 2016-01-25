662e430198c26e61fa76b046df188d1c0ad46d4a
Joseph Karasek and rob reasoner <reasoner55@hotmail.com>
2016-01-25 15:18:13 -0800

    add function populate result list with name of inputted place

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ function Place(name,location,description) {
   this.placeLocation = location;
   this.placeDescription = description;
 }
+
+
+
+$(document).ready(function() {
+
+  $('#placeForm').submit(function(event) {
+    event.preventDefault();
+
+    var placeName = $('#placeNameInput').val();
+    var placeLocation = $('#placeLocationInput').val();
+    var placeDescription = $('#placeDescriptionInput').val();
+
+    var newPlace = new Place(placeName, placeLocation, placeDescription);
+
+    $('#resultList').append("<li>"+ newPlace.placeName +"</li>");
+  });
+});
